e9ae78c4ae6d2856125d112485f52c7359d42f7e
JohannLee1996 <johannlee@qq.com>
2019-03-03 17:26:00 -0400

    bar chart with dynamic data

--- map.js
+++ map.js
@@ -22,7 +22,9 @@ function all(geojson, csvdata) {
         .attr('class', 'gmap')
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var year = '2008';
+
+    var year = {value: '2008'};
+    var countryCode = {value: 'CHN'}
 
     svg.append('text')
         .attr('id', 'info')
@@ -41,6 +43,22 @@ function all(geojson, csvdata) {
     var myColor = d3.scaleSequential(d3.interpolateInferno)
         .domain([0, 100000000000])
 
+    var bardata = [];
+
+    for (let i = 0; i < 10; i++) {
+        bardata.push({});
+    }
+
+    var currentYearData = [];
+
+    for (let i = 0; i < csvdata.length; i++) {
+        currentYearData.push({});
+    }
+
+    getCurrentYearData(csvdata, year, currentYearData)
+    getBarData(countryCode, currentYearData, bardata)
+
+
     d3.select('.gmap')
         .selectAll('path')
         .data(geojson.features)
@@ -49,50 +67,37 @@ function all(geojson, csvdata) {
         .attr('class', 'countries')
         .attr('d', geoGenerator)
         .attr('fill', function (d) {
-            const country = csvdata.filter(dj => dj['Country Code'] == d.properties.ADM0_A3)
+            const country = currentYearData.filter(dj => dj.code == d.properties.ADM0_A3)
             if (country.length == 0) {
                 return myColor(0)
             } else {
-                const dataGDP = country[0][year];
+                const dataGDP = country[0].GDP;
                 return myColor(dataGDP)
             }
         })
         .style('stroke', 'white')
         .style('stroke-width', '0.1')
         .on('mouseover', function (d) {
-            const country = csvdata.filter(dj => dj['Country Code'] == d.properties.ADM0_A3)
+            const country = currentYearData.filter(dj => dj.code == d.properties.ADM0_A3)
             let dataGDP = '';
-            if (country.length == 0) {
-                dataGDP = ''
-            } else {
-                dataGDP = country[0][year];
+            if (country.length != 0) {
+                dataGDP = country[0].GDP;
             }
             d3.select('#info')
                 .text(function () {
                     if (dataGDP != '') {
-                        return d.properties.ADMIN + " (" + year + "): " + dataGDP;
-                    } else return d.properties.ADMIN + " (" + year + ") : Not available."
+                        return d.properties.ADMIN + " (" + year.value + "): " + dataGDP;
+                    } else return d.properties.ADMIN + " (" + year.value + ") : Not available."
                 })
 
         })
         .on('mouseout', function () {
             // d3.select('#info').remove();
         })
-        .on('click', function () {
-            let fakedata2 = [
-                {"Country Name": 'CHN', "GDP": 5},
-                {"Country Name": 'USA', "GDP": 2},
-                {"Country Name": 'UK', "GDP": 3},
-                {"Country Name": 'RUS', "GDP": 4},
-                {"Country Name": 'KRA', "GDP": 5},
-                {"Country Name": 'JPN', "GDP": 6},
-                {"Country Name": 'GRM', "GDP": 7},
-                {"Country Name": 'FRC', "GDP": 8},
-                {"Country Name": 'IND', "GDP": 9},
-                {"Country Name": 'CAN', "GDP": 10},
-            ];
-
-            barUpdate(fakedata2)
+        .on('click', function (d) {
+            countryCode.value = d.properties.ADM0_A3;
+            getBarData(countryCode, currentYearData, bardata)
+            barUpdate(bardata)
         })
 
     //Drag Slider
@@ -106,6 +111,7 @@ function all(geojson, csvdata) {
     var slider = svg.append("g")
         .attr("class", "slider")
         .attr("transform", "translate(" + margin.left + "," + (height + 30) + ")");
+
 
     slider.append("line")
         .attr("class", "track")
@@ -124,8 +130,11 @@ function all(geojson, csvdata) {
                 slider.interrupt();
             })
             .on("start drag", function () {
-                hue(x.invert(d3.event.x));
-                updateColor()
+                updateSlider(x.invert(d3.event.x));
+                getCurrentYearData(csvdata, year, currentYearData)
+                updateColor(currentYearData, myColor)
+                getBarData(countryCode, currentYearData, bardata)
+                barUpdate(bardata)
             }));
 
     slider.insert("g", ".track-overlay")
@@ -144,52 +153,59 @@ function all(geojson, csvdata) {
         .attr("class", "handle")
         .attr("r", 9);
 
-
-    function updateColor() {
-        d3.selectAll('.countries')
-            .each(function () {
-                d3.select(this)
-                    .attr('fill', function (d) {
-                        const country = csvdata.filter(dj => dj['Country Code'] == d.properties.ADM0_A3)
-                        if (country.length == 0) {
-                            return myColor(0)
-                        } else {
-                            const dataGDP = country[0][year];
-                            return myColor(dataGDP)
-                        }
-                    })
-            })
-
-    }
-
-    function hue(h) {
+    function updateSlider(h) {
         handle.attr("cx", x(h));
-        year = Math.round(h)
+        year.value = Math.round(h)
     }
 
 
-    //bar chart
+    d3.select("#play-button")
+        .text('Play')
+        .on("click", function () {
+            var button = d3.select(this);
+            if (button.text() == "Pause") {
+                moving = false;
+                // timer = 0;
+                button.text("Play");
+            } else {
+                moving = true;
+                button.text("Pause");
+                press()
+            }
+        })
 
-    var fakedata = [
-        {"Country Name": 'CHN', "GDP": 1},
-        {"Country Name": 'USA', "GDP": 2},
-        {"Country Name": 'UK', "GDP": 3},
-        {"Country Name": 'RUS', "GDP": 4},
-        {"Country Name": 'KRA', "GDP": 5},
-        {"Country Name": 'JPN', "GDP": 6},
-        {"Country Name": 'GRM', "GDP": 7},
-        {"Country Name": 'FRC', "GDP": 8},
-        {"Country Name": 'IND', "GDP": 9},
-        {"Country Name": 'CAN', "GDP": 10},
-    ];
+//bar chart
 
+    bar(bardata)
+}
 
+function press() {
+    setInterval(console.log('press'), 1000)
+}
 
-    bar(fakedata)
+function release() {
+
+}
+
+function updateColor(currentYearData, myColor) {
+    d3.selectAll('.countries')
+        .each(function () {
+            d3.select(this)
+                .attr('fill', function (d) {
+                    const country = currentYearData.filter(dj => dj.code == d.properties.ADM0_A3)
+                    if (country.length == 0) {
+                        return myColor(0)
+                    } else {
+                        const dataGDP = country[0].GDP;
+                        return myColor(dataGDP)
+                    }
+                })
+        })
+
 }
 
 
-function bar(fakedata) {
+function bar(bardata) {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
 
@@ -211,9 +227,9 @@ function bar(fakedata) {
 
     let arrCountry = [];
     let arrGDP = [];
-    for (let i = 0; i < fakedata.length; i++) {
-        arrCountry.push(fakedata[i]['Country Name'])
-        arrGDP.push(fakedata[i]['GDP'])
+    for (let i = 0; i < bardata.length; i++) {
+        arrCountry.push(bardata[i]['Country Code'])
+        arrGDP.push(bardata[i]['GDP'])
     }
 
     var xMax = d3.max(arrGDP)
@@ -221,8 +237,8 @@ function bar(fakedata) {
     var rectPadding = 10;
 
     let arrRange = [];
-    for(let i=0;i<=fakedata.length;i++){
-        arrRange.push((i)*height/fakedata.length)
+    for (let i = 0; i <= bardata.length; i++) {
+        arrRange.push((i) * height / bardata.length)
     }
 
     var x = d3.scaleLinear()
@@ -246,6 +262,7 @@ function bar(fakedata) {
 
 
     g.append("g")
+        .attr('id', 'barX')
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
         .selectAll("text")
@@ -255,6 +272,7 @@ function bar(fakedata) {
         .style("font-size", "11px");
 
     g.append("g")
+        .attr('id', 'barY')
         .call(yAxis)
         .selectAll("text")
         .attr("y", rectPadding)
@@ -263,26 +281,129 @@ function bar(fakedata) {
         .style("text-anchor", "end")
         .style("font-size", "10px");
 
-    g.selectAll('rect')
-        .data(fakedata)
+    g.selectAll('.bars')
+        .data(bardata)
         .enter()
         .append('rect')
-        .attr('x',0)
+        .attr('class', 'bars')
+        .attr('x', 0)
         .attr('y', function (d, i) {
-            return i * (height / fakedata.length);
+            return i * (height / bardata.length);
         })
         .attr('width', function (d) {
             return (d.GDP / xMax) * width;
         })
         .attr('height', function () {
-            return height / fakedata.length - rectPadding;
+            return height / bardata.length - rectPadding;
         })
-        .attr('fill', 'red')
+        .attr('fill', function (d, i) {
+            if (i == 4) {
+                return 'orange'
+            } else {
+                return 'red'
+            }
+        })
 }
 
 
-function barUpdate(fakedata){
+function barUpdate(bardata) {
+    var rectPadding = 10;
 
+    let arrCountry = [];
+    let arrGDP = [];
+    for (let i = 0; i < bardata.length; i++) {
+        arrCountry.push(bardata[i]['Country Code'])
+        arrGDP.push(bardata[i]['GDP'])
+    }
+
+    let arrRange = [];
+    for (let i = 0; i <= bardata.length; i++) {
+        arrRange.push((i) * height / bardata.length)
+    }
+
+    var xMax = d3.max(arrGDP)
+
+    var x = d3.scaleLinear()
+        .domain([0, xMax])
+        .range([0, width]);
+
+    var y = d3.scaleOrdinal()
+        .domain(arrCountry)
+        .range(arrRange);
+
+    var xAxis = d3.axisBottom(x)
+        .tickSize(5)
+        .ticks(10)
+
+    var yAxis = d3.axisRight(y)
+        .tickSize(0)
+        .ticks(11)
+        .tickFormat(function (d) {
+            return d;
+        });
+
+    d3.select('#barX')
+        .transition()
+        .duration(800)
+        .call(xAxis)
+        .selectAll("text")
+        .attr("y", 8)
+        .attr('class', 'bree')
+        .style("text-anchor", "center")
+        .style("font-size", "11px");
+
+    d3.select('#barY')
+        .transition()
+        .duration(800)
+        .call(yAxis)
+        .selectAll("text")
+        .attr("y", rectPadding)
+        .attr("x", -6)
+        .attr('class', 'bree')
+        .style("text-anchor", "end")
+        .style("font-size", "10px");
+
+    d3.selectAll('.bars')
+        .data(bardata)
+        .each(function () {
+            d3.select(this)
+                .transition()
+                .duration(800)
+                .attr('width', function (d) {
+                    return (d.GDP / xMax) * width;
+                })
+        })
+}
+
+function getBarData(countryCode, currentYearData, bardata) {
+    let country = currentYearData.filter(dj => dj.code == countryCode.value);
+    let index = currentYearData.indexOf(country[0])
+    if (index < 5) {
+        updateBarData(0)
+    } else if (currentYearData.length - index < 5) {
+        updateBarData(currentYearData.length - 10)
+    } else {
+        updateBarData(index - 4)
+    }
+
+    function updateBarData(index) {
+        for (let i = 0; i < 10; i++) {
+            bardata[i] = {'Country Code': currentYearData[index + i].code, "GDP": currentYearData[index + i].GDP};
+        }
+    }
+}
+
+function getCurrentYearData(csvdata, year, currentYearData) {
+    let toBeSort = [];
+    for (let i = 0; i < csvdata.length; i++) {
+        toBeSort.push([csvdata[i]['Country Name'], csvdata[i]['Country Code'], csvdata[i][year.value]]);
+    }
+    toBeSort.sort(function (a, b) {
+        return b[2] - a[2];
+    })
+    for (let i = 0; i < csvdata.length; i++) {
+        currentYearData[i] = {name: toBeSort[i][0], code: toBeSort[i][1], GDP: toBeSort[i][2]};
+    }
 }
 
 
