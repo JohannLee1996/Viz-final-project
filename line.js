81510cc31ab05a3460c79cf5c4deae70ddecc117
JohannLee1996 <johannlee@qq.com>
2019-03-21 17:56:01 -0300

    bubble chart done

--- line.js
+++
@@ -1,9 +0,0 @@
-windowWidth = window.innerWidth;
-windowHeight = window.innerHeight;
-
-var svg = d3.select("#divline")
-    .append("svg")
-    .attr('id', 'svgmap')
-    .attr("width", windowWidth)
-    .attr("height", windowWidth / 4)
-    .style('background-color', '#005555');
--- map.js
+++ map.js
@@ -40,8 +40,8 @@ function all(geojson, csvdata) {
         .projection(projection);
 
 
-    var myColor = d3.scaleSequential(d3.interpolateInferno)
-        .domain([0, 100000000000])
+    var myColor = d3.scaleSequential(d3.interpolateBlues)
+        .domain([0, 19390604000000])
 
     var bardata = [];
 
@@ -50,15 +50,19 @@ function all(geojson, csvdata) {
     }
 
     var currentYearData = [];
+    var bubbleData = [];
 
     for (let i = 0; i < csvdata.length; i++) {
         currentYearData.push({});
+        bubbleData.push({});
     }
+
+    var linedata = [{}];
 
     getCurrentYearData(csvdata, year, currentYearData)
     getBarData(countryCode, currentYearData, bardata)
-
-    console.log(currentYearData)
+    getBubbleData(currentYearData, bubbleData)
+    getCurrentCountryData(csvdata, countryCode, linedata)
 
 
     d3.select('.gmap')
@@ -137,6 +141,8 @@ function all(geojson, csvdata) {
                 updateColor(currentYearData, myColor)
                 getBarData(countryCode, currentYearData, bardata)
                 barUpdate(bardata)
+                getBubbleData(currentYearData, bubbleData)
+                bubbleUpdate(bubbleData);
             }));
 
     slider.insert("g", ".track-overlay")
@@ -167,22 +173,210 @@ function all(geojson, csvdata) {
             var button = d3.select(this);
             if (button.text() == "Pause") {
                 moving = false;
-                // timer = 0;
+                clearInterval(window.timer1);
                 button.text("Play");
             } else {
                 moving = true;
                 button.text("Pause");
-                press()
+                var i = 0;
+
+                function timer() {
+                    console.log('mmm');
+                }
+
+                window.timer1 = setInterval(timer(), 1000);
             }
         })
 
 //bar chart
 
     bar(bardata)
+
+
+    //bubble chart
+
+    bubble(bubbleData)
+
+    //line chart
+
+    line(linedata)
 }
 
-function press() {
-    setInterval(console.log('press'), 1000)
+function getCurrentCountryData(csvdata, countryCode, linedata){
+    let country = csvdata.filter(dj => dj['Country Code'] == countryCode.value);
+    linedata[0] = country;
+}
+
+function line(linedata){
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    var svg = d3.select("#divline")
+        .append("svg")
+        .attr('id', 'svgmap')
+        .attr("width", windowWidth)
+        .attr("height", windowWidth / 4)
+        .style('background-color', '#eeeeee');
+
+    var margin = {left: 100 ,right: 50,top: 20, bottom: 50}
+    var width = windowWidth - margin.left - margin.right;
+    var height = windowWidth/4 - margin.top - margin.bottom;
+
+    var g = svg.append('g')
+        .attr('transform','translate('+margin.left+','+margin.top+')')
+
+    var data = []
+    for(let i=1960;i<2018;i++){
+        data.push(linedata[0][0][d3.format("")(i)])
+    }
+
+    const xMax = d3.max(data)
+    console.log(xMax)
+
+    var y = d3.scaleLinear()
+        .domain([0, xMax])
+        .range([height,0]);
+
+    var x = d3.scaleTime()
+        .domain([new Date(1959, 9, 1), new Date(2019, 3, 1)])
+        .range([0,width])
+
+    var xAxis = d3.axisBottom(x)
+        .tickSize(5)
+        .ticks(10)
+
+    var yAxis = d3.axisRight(y)
+        .tickSize(width)
+        .ticks(11)
+        .tickFormat(function (d) {
+            return d;
+        });
+
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("y", 8)
+        .attr('class', 'bree')
+        .style("text-anchor", "center")
+        .style("font-size", "11px");
+
+    g.append("g")
+        .call(yAxis)
+        .selectAll("text")
+        // .attr("y", 0)
+        .attr("x", -6)
+        .attr('class', 'bree')
+        .style("text-anchor", "end")
+        .style("font-size", "10px");
+
+}
+
+function bubble(bubbleData) {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+
+    var svg = d3.select("#divbubble")
+        .append("svg")
+        .attr('id', 'svgmap')
+        .attr("width", (windowWidth / 2) - 10)
+        .attr("height", windowWidth / 4);
+
+    var data = {'children': bubbleData};
+
+    var color = d3.scaleSequential(d3.interpolateBlues)
+        .domain([0, 40])
+
+    var bubble = d3.pack(data)
+        .size([(windowWidth / 2) - 12, (windowWidth / 4) - 2])
+        .padding(1.5);
+
+    var nodes = d3.hierarchy(data)
+        .sum(function (d) {
+            return d.GDP;
+        });
+
+    var node = svg.selectAll(".node")
+        .data(bubble(nodes).descendants())
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    node.append("title")
+        .attr('class', 'bubbleTitle')
+        .text(function (d) {
+            return d.data.name + ": " + d.data.GDP;
+        });
+
+    node.append("circle")
+        .attr('class', 'bubbleCircle')
+        .attr("r", function (d) {
+            return d.r;
+        })
+        .style("fill", function (d) {
+            return color(d.r);
+        });
+
+    d3.select(self.frameElement)
+        .style("height", (windowWidth / 4) - 2 + "px");
+
+}
+
+function bubbleUpdate(bubbleData) {
+
+    var data = {'children': bubbleData};
+
+    var color = d3.scaleSequential(d3.interpolateBlues)
+        .domain([0, 40])
+
+    var bubble = d3.pack(data)
+        .size([(windowWidth / 2) - 12, (windowWidth / 4) - 2])
+        .padding(1.5);
+
+    var nodes = d3.hierarchy(data)
+        .sum(function (d) {
+            return d.GDP;
+        });
+
+    d3.selectAll(".node")
+        .data(bubble(nodes).descendants())
+        .each(function () {
+            d3.select(this)
+                .transition()
+                .duration(800)
+                .attr("transform", function (d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+        })
+
+    d3.selectAll(".bubbleCircle")
+        .data(bubble(nodes).descendants())
+        .each(function () {
+            d3.select(this)
+                .transition()
+                .duration(800)
+                .attr('r',function(d){
+                    return d.r;
+                })
+                .style("fill", function (d) {
+                    return color(d.r);
+                });
+        })
+
+    d3.selectAll(".bubbleTitle")
+        .data(bubble(nodes).descendants())
+        .each(function () {
+            d3.select(this)
+                .text(function (d) {
+                    return d.data.name + ": " + d.data.GDP;
+                })
+        })
+
+
 }
 
 function release() {
@@ -405,6 +599,12 @@ function getCurrentYearData(csvdata, year, currentYearData) {
     })
     for (let i = 0; i < csvdata.length; i++) {
         currentYearData[i] = {name: toBeSort[i][0], code: toBeSort[i][1], GDP: toBeSort[i][2]};
+    }
+}
+
+function getBubbleData(currentYearData, bubbleData) {
+    for (let i = 0; i < currentYearData.length; i++) {
+        bubbleData[i] = currentYearData[i];
     }
 }
 
